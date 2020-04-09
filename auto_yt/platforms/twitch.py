e09dcd6b15d3b905611b299186a2cd111e706392
JoshGuarino <guar0638@fredonia.edu>
2020-04-09 14:26:58 -0400

    able to acquire and store access token in config

--- auto_yt/platforms/twitch.py
+++ auto_yt/platforms/twitch.py
@@ -1,28 +1,76 @@
 import requests
 import yaml
+import os
+import json
 
 class Twitch:
     def __init__(self, client_id, client_secret, access_token):
-        self.base_path = 'https://api.twitch.tv/helix/'
+        self.main_path = 'https://api.twitch.tv/helix/'
+        self.oauth2_path = 'https://id.twitch.tv/oauth2/'
+        self.root_path = os.getcwd()
         self.game = None
         self.client_id = client_id
         self.client_secret = client_secret
         self.access_token = access_token
 
-    def get_access_token():
+
+    def get(self, url, payload, headers):
+        response = requests.get(url, params=payload, headers=headers)
+        if response.status_code == 401:
+            data = json.loads(response.text)
+            self.acquire_access_token()
+            return data
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            return data
+        print(response.text)
+        exit(1)
+
+
+    def post(self, url, payload, headers):
+        response = requests.post(url, params=payload, headers=headers)
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            return data
+        print(response.text)
+        exit(1) 
+
+
+    def acquire_access_token(self):
+        url = '{}token'.format(self.oauth2_path)
+        payload = { 'client_id':self.client_id, 'client_secret':self.client_secret, 'grant_type':'client_credentials' }
+        data = self.post(url, payload, {})
+        print(data)
+        config_path = '{}/config.yaml'.format(self.root_path)
+        with open(config_path) as f:
+            config_data = yaml.safe_load(f)    
+        config_data['twitch']['access-token'] = data['access_token']
+        self.access_token = data['access_token']
+        with open('config.yaml', 'w') as f:
+            yaml.safe_dump(config_data, f)
         return
 
-    def check_token_valid(): 
+
+    def check_token_valid(self):
+        url = '{}validate'.format(self.oauth2_path)
+        headers = {'Authorization' : 'OAuth {}'.format(self.access_token)}
+        print(headers)
+        data = self.get(url, {}, headers)
+        print(data)
         return
 
-    def revoke_access_token():
+
+    def revoke_access_token(self):
         return
 
-    def get_game():
+
+    def get_game_id(self):
         return
 
-    def get_clips():
+
+    def get_clips(self):
         return
+
 
     def test(self):
-        print(self.client_id, self.client_secret, self.access_token)
+        self.check_token_valid()
--- run.py
+++ run.py
@@ -3,8 +3,11 @@ from auto_yt.platforms.twitch import Twitch
 
 with open('config.yaml') as f:
     data = yaml.safe_load(f)
-    print(data)
+    # print(data)
 
 twitch_config = data['twitch']
 twitch = Twitch(twitch_config['client-id'], twitch_config['client-secret'], twitch_config['access-token'])
 twitch.test()
+
+# with open('config.yaml', 'w') as f:
+#     yaml.safe_dump(data, f)
