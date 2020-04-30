d8853931b5f6dc2fbce0b1587a1824fe456736e3
JoshGuarino <guar0638@fredonia.edu>
2020-04-30 15:23:24 -0400

    converted format() to f strings

--- auto_yt/platforms/twitch.py
+++ auto_yt/platforms/twitch.py
@@ -42,35 +42,35 @@ class Twitch:
 
 
     def acquire_access_token(self):
-        url = '{}token'.format(self.oauth2_path)
+        url = f'{self.oauth2_path}token'
         payload = { 'client_id':self.client_id, 'client_secret':self.client_secret, 'grant_type':'client_credentials' }
         data = self.post(url, payload, {})
-        config_path = '{}/config.yaml'.format(self.root_path)
+        config_path = f'{self.root_path}/config.yaml'
         with open(config_path) as f:
             config_data = yaml.safe_load(f)    
         config_data['twitch']['access-token'] = data['access_token']
         self.access_token = data['access_token']
         with open('config.yaml', 'w') as f:
             yaml.safe_dump(config_data, f)
-        print('Acquired access token: {}'.format(data['access_token']))
+        print(f'Acquired access token: {self.access_token}')
         return
 
 
     def check_token_valid(self):
-        url = '{}validate'.format(self.oauth2_path)
-        headers = { 'Authorization' : 'OAuth {}'.format(self.access_token) }
-        print('Checking validity of token: {}'.format(self.access_token))
+        url = f'{self.oauth2_path}validate'
+        headers = { 'Authorization' : f'OAuth {self.access_token}' }
+        print(f'Checking validity of token: {self.access_token}')
         data = self.get(url, {}, headers)
         print('Token is valid.')
         return
 
 
     def revoke_access_token(self):
-        url = '{}revoke'.format(self.oauth2_path)
+        url = f'{self.oauth2_path}revoke'
         payload = { 'client_id':self.client_id, 'token':self.access_token }
-        print('Revoking access token: {}'.format(self.access_token))
+        print(f'Revoking access token: {self.access_token}')
         data = self.post(url, payload, {})
-        print('Successfully revoked access token: {}'.format(self.access_token))
+        print(f'Successfully revoked access token: {self.access_token}')
         return
 
 
@@ -79,8 +79,8 @@ class Twitch:
         if not os.path.exists(clips_url):
             os.makedirs(clips_url)
         for clip in progressbar.progressbar(self.clips, prefix='Downloading clips: '):
-            url = '{}.mp4'.format(clip['thumbnail_url'][0:-20])
-            out_path = '{}/auto_yt/clips/{}'.format(self.root_path, clip['video_id'])
+            url = f'{clip["thumbnail_url"][0:-20]}.mp4'
+            out_path = f'{clips_url}{clip["video_id"]}'
             response = requests.get(url, stream=True)
             with open(out_path, 'wb') as out_file:
                 shutil.copyfileobj(response.raw, out_file)         
@@ -88,8 +88,8 @@ class Twitch:
 
 
     def get_clips(self, date, game_id, num_of_clips):
-        url = '{}clips'.format(self.main_path)
-        headers = { 'Authorization' : 'OAuth {}'.format(self.access_token), 'Client-ID' : self.client_id }
+        url = f'{self.main_path}clips'
+        headers = { 'Authorization' : f'OAuth {self.access_token}', 'Client-ID' : self.client_id }
         payload = { 'started_at':date, 'game_id': game_id, 'first':num_of_clips }
         data = self.get(url, payload, headers)
         return data['data']
