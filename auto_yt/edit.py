c7771b812a884bcf5b38eb3f8a92d1fe64d22fe2
Josh Guarino <jguarino722@gmail.com>
2024-10-31 18:26:27 -0400

    fix edit functionality and add cleanup method

--- auto_yt/edit.py
+++ auto_yt/edit.py
@@ -5,12 +5,15 @@ class Edit:
     def __init__(self):
         self.clip_dir = f'{os.getcwd()}/auto_yt/clips'
 
-    def concat_clips(self, sub_dir):
+    def concat_clips(self):
         clip_list = []
         for file in os.listdir(self.clip_dir):
-            clip_list.append(VideoFileClip(f'{self.clip_dir}/{sub_dir}/{file}'))
+            clip_list.append(VideoFileClip(f'{self.clip_dir}/{file}'))
         return concatenate_videoclips(clip_list)
-
 
     def write_final_video(self, final_clip, write_dir):
         final_clip.write_videofile(write_dir)
+
+    def cleanup_clips_dir(self):
+        for file in os.listdir(self.clip_dir):
+            os.remove(file)
