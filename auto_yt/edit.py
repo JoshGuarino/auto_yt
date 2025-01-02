c54ecc89862e2d6c71695e5d1d56cad4d0135ef6
Josh Guarino <jguarino722@gmail.com>
2025-01-02 12:17:38 -0500

    push uncommited code, not sure of state of this repo

--- auto_yt/edit.py
+++ auto_yt/edit.py
@@ -8,8 +8,10 @@ class Edit:
     def concat_clips(self):
         clip_list = []
         for file in os.listdir(self.clip_dir):
-            clip_list.append(VideoFileClip(f'{self.clip_dir}/{file}'))
-        return concatenate_videoclips(clip_list)
+            # clip_list.append(VideoFileClip(f'{self.clip_dir}/{file}'))
+            print(f'{file}')
+            return VideoFileClip(f'{self.clip_dir}/{file}')
+        # return concatenate_videoclips(clip_list)
 
     def write_final_video(self, final_clip, write_dir):
         final_clip.write_videofile(write_dir)
